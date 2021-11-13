6c88dea9d5b6e4544feb0d93709da33121efe883
Devesh <rockdev1234560@gmail.com>
2021-11-13 16:54:23 +0530

    initialise Form input of ID and use value of the form (based on event objects) and refactored some UI

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Counter from './counter.js'
-import {Jumbotron, Container, Row, Col, Media} from 'react-bootstrap';
+import {Jumbotron, Container, Row, Col, Media, Form, InputGroup, Button} from 'react-bootstrap';
+import { red } from '@material-ui/core/colors';
 
 export default (props) => {
+
+    const [ID, setID] = useState('');
+    const onFill=(e)=>{
+        console.log(e);
+    }
+
     return(
         <Jumbotron fluid className="p-2">
         <Container fluid>
@@ -17,7 +24,12 @@ export default (props) => {
                     </Media>
                 </Col>
                 <Col md={{span:5,offset:1}} lg={{span:4,offset:2}}>
-                    <Counter server={props.server} counter={props.counter} getCount={props.getCount}/>
+                        <InputGroup className="mt-2">
+                            <Form.Control id="bitsid" placeholder="BITS ID" value={ID} onChange={(e)=>{setID(e.target.value); console.log(e.target.value)}}/>
+                            <InputGroup.Append>
+                                <Button style={{backgroundColor: "red"}} onSubmit={(e)=>{e.preventDefault();}}>Enter your BITS ID</Button>
+                            </InputGroup.Append>
+                        </InputGroup>
                 </Col>            
             </Row>
         </Container>
